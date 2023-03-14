192fd179601acf52d5b255e808ac9731ebbad984
Laumonier <timothee.laumonier@gmail.com>
2023-03-14 10:54:09 +0100

    Première version

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carre
+{
+    class Program
+    {
+        static void Main(int i)
+        {
+            Console.WriteLine("Son carré fait : %d",i*i);
+        }
+    }
+}
